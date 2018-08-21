cfbe84de6af446bec6d4cd0030ce89d34d30a508
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2018-08-21 15:56:39 -0400

    deploy

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,5 @@
 const {resolve} = require('path')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-let FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -28,23 +26,7 @@ module.exports = {
     ],
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({title: 'interferethometer', template: './src/app/index.html'}),
-    new FaviconsWebpackPlugin({
-      logo: './src/app/static/favicon.png',
-      prefix: 'icons-[hash]/',
-      emitStats: false,
-      statsFilename: 'iconstats-[hash].json',
-      persistentCache: true,
-      inject: true,
-      background: '#fff',
-      title: 'template-favicon',
-      icons: {
-        favicons: true,
-        firefox: true,
-        twitter: true
-      }
-    })
   ],
   output: {
     filename: '[name].[hash].bundle.js',
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,15 +1,17 @@
 const merge = require('webpack-merge')
 const webpack = require('webpack')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist'
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin()
-    ]
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ]
 })
