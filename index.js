8b87d1e752ecb4941c5fe56f9952c3bc44bae89a
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2018-08-21 18:18:36 -0400

    deploy

--- index.js
+++ index.js
@@ -8,17 +8,19 @@ const dev = require('./webpack.dev')
 const prod = require('./webpack.prod')
 
 
-let config = process.env.NODE_ENV === 'production' ? prod : dev
+let config = process.env.NODE_ENV === 'development' ? dev : prod
 const port = process.env.PORT || 3000
 
 const compiler = webpack(config)
 const app = express()
+if (process.env.NODE_ENV === 'development') {
+  app
+    .use(hmr(compiler))
+}
 app
-  .use(hmr(compiler))
   .use(webpackDevMiddleware(compiler, {
     noInfo: true,
     publicPath: config.output.publicPath
   }))
   .use('/*', express.static(resolve(`${__dirname}/index.html`)))
-
-app.listen(port, () => console.log(`Example app listening on port: ${port}!`))
+  .listen(port, () => console.log(`Example app listening on port: ${port}!`))
